ed9c9f4f12cc467d3893087e9881afa64526ec00
Tianguanjun <1411225015@qq.com>
2018-04-26 22:13:37 +0800

    hide and go seek
    This is a game that we find each other using ANT channels.

--- firmware_common/application/user_app1.h
+++ firmware_common/application/user_app1.h
@@ -29,7 +29,29 @@ Type Definitions
 Constants / Definitions
 **********************************************************************************************************************/
 
-
+#define ANT_CHANNEL_SLAVE                ANT_CHANNEL_0         
+#define ANT_CHANNEL_TYPE_SLAVE           CHANNEL_TYPE_SLAVE    
+#define ANT_CHANNEL_MASTER               ANT_CHANNEL_1         
+#define ANT_CHANNEL_TYPE_MASTER          CHANNEL_TYPE_MASTER 
+#define ANT_DEVICE0ID_LO_USERAPP         (u8)0x35              
+#define ANT_DEVICE0ID_HI_USERAPP         (u8)0x25
+#define ANT_DEVICE_TYPE_USERAPP          (u8)1        
+#define ANT_TRANSMISSION_TYPE_USERAPP    (u8)1             
+#define ANT_CHANNEL_PERIOD_LO_USERAPP    (u8)0x00       
+#define ANT_CHANNEL_PERIOD_HI_USERAPP    (u8)0x20      
+#define ANT_FREQUENCY_USERAPP            (u8)50           
+#define ANT_TX_POWER_USERAPP             RADIO_TX_POWER_4DBM
+#define TIMEOUT_VALUE                    (u16)3000
+#define HIDING_TIME_COUNTER              (u8)10
+#define HIDING_TIME_DIS_PLACE            (u8)15
+#define ONE_SECOND_TIME                  (u16)1000
+#define TOTAL_LEDS_COUNT                 (u8)8
+#define MAXIMUM_RSSI                     (s8)-40
+#define WAIT_HIDING                      (u8)0
+#define IS_SEARCHING                     (u8)1
+#define FIND_HIM                         (u8)2
+#define KEY_PARAMETER                    (u8)0x12
+#define EACH_TONE_TIME                   (u16)400
 /**********************************************************************************************************************
 Function Declarations
 **********************************************************************************************************************/
@@ -54,7 +76,24 @@ void UserApp1RunActiveState(void);
 /***********************************************************************************************************************
 State Machine Declarations
 ***********************************************************************************************************************/
-static void UserApp1SM_Idle(void);    
+static void UserApp1SM_Idle(void);
+
+static fnCode_type UserApp1_StateMachine;            /* The state machine function pointer */
+static u32 UserApp1_u32Timeout;                      /* Timeout counter used across states */
+static AntAssignChannelInfoType sAntSetupData;
+static bool bChannelIsMaster = TRUE;
+
+
+extern u32 G_u32AntApiCurrentDataTimeStamp;                       
+extern AntApplicationMessageType G_eAntApiCurrentMessageClass;   
+extern u8 G_au8AntApiCurrentMessageBytes[ANT_APPLICATION_MESSAGE_BYTES];
+extern AntExtendedDataType G_sAntApiCurrentMessageExtData; 
+
+
+extern volatile u32 G_u32SystemFlags;                  /* From main.c */
+extern volatile u32 G_u32ApplicationFlags;             /* From main.c */
+
+extern volatile u32 G_u32SystemTime1ms;                /* From board-specific source file */
 
 static void UserApp1SM_Error(void);         
 
